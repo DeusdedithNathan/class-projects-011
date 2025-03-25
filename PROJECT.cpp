fd792726f0f521474e4d0ad681456ece5fda0715
DeusdedithNathan <deusd2070@gmail.com>
2025-03-26 01:04:30 +0300

    STARTING THE PROJECT

--- PROJECT.cpp
+++ PROJECT.cpp
@@ -2,3 +2,14 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
+class Stock {
+public:
+    std::string name;
+    float price;
+
+    Stock(std::string stockName, float stockPrice) : name(stockName), price(stockPrice) {}
+
+    void updatePrice(float newPrice) {
+        price = newPrice;
+    }
+};
