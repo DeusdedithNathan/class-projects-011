2c15fa325b07ea095b4ff16ea5b7a117f553d190
DeusdedithNathan <deusd2070@gmail.com>
2025-03-27 00:58:11 +0300

    Update PROJECT.cpp

--- PROJECT.cpp
+++ PROJECT.cpp
@@ -13,3 +13,12 @@ public:
         price = newPrice;
     }
 };
+class Portfolio {
+private:
+    std::vector<std::pair<Stock, int>> stocks; // Stock and quantity
+
+public:
+    void buyStock(const Stock& stock, int quantity) {
+        stocks.push_back(std::make_pair(stock, quantity));
+        std::cout << "Bought " << quantity << " shares of " << stock.name << " at $" << stock.price << "." << std::endl;
+    }
